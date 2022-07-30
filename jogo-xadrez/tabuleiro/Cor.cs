7f9f7dc73fcd3b24e893eb81043a6ce07fcc5e6d
Lucas Moura <lucasdasilvmoura@gmail.com>
2022-07-30 17:12:05 -0300

    Implementação de um metado para imprir o tabuleiro na tela

--- jogo-xadrez/tabuleiro/Cor.cs
+++ jogo-xadrez/tabuleiro/Cor.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace jogo_xadrez.tabuleiro
+﻿namespace tabuleiro
 {
-    internal class Cor
+    enum Cor
     {
+
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja
     }
 }
